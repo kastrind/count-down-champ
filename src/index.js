3dcc467b5249e9106b1a90d0e3a1e18fa73aa727
Dimitris <kastrind@gmail.com>
2017-11-15 16:03:05 +0200

    Added frontend and some styling.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import React from 'react';
-import React from 'react-dom';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />, document.getElementById('root')
+);
